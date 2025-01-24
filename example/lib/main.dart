0f8d2e09047a44f6a070899c5d7dbdf053b25735
drixed <59870207+drixed@users.noreply.github.com>
2025-01-24 13:20:35 +0500

    test

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,69 +1,69 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'dart:async';
 
-import 'package:flutter/services.dart';
-import 'package:rees46_plugin/rees46_plugin.dart';
+// import 'package:flutter/services.dart';
+// import 'package:rees46_plugin/rees46_plugin.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+// void main() {
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+// class MyApp extends StatefulWidget {
+//   const MyApp({super.key});
 
-  @override
-  State<MyApp> createState() => _MyAppState();
-}
+//   @override
+//   State<MyApp> createState() => _MyAppState();
+// }
 
-class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _rees46Plugin = Rees46Controller.instance();
+// class _MyAppState extends State<MyApp> {
+//   String _platformVersion = 'Unknown';
+//   final _rees46Plugin = Rees46Controller.instance();
 
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     initPlatformState();
+//   }
 
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      _rees46Plugin.initialize('d538197df225c66e7413647e553289', '');
-      _rees46Plugin.initialize('c58471e2acfbc83c9f02d1083a6b07a5', '');
-      _rees46Plugin.initialize('7e6c2ef77fcc6bc80fedf64a7103b705', '');
-      _rees46Plugin.initialize('da2668fe479fa866e9d888d976db914a', '');
+//   // Platform messages are asynchronous, so we initialize in an async method.
+//   Future<void> initPlatformState() async {
+//     String platformVersion;
+//     // Platform messages may fail, so we use a try/catch PlatformException.
+//     // We also handle the message potentially returning null.
+//     try {
+//       _rees46Plugin.initialize('d538197df225c66e7413647e553289', '');
+//       _rees46Plugin.initialize('c58471e2acfbc83c9f02d1083a6b07a5', '');
+//       _rees46Plugin.initialize('7e6c2ef77fcc6bc80fedf64a7103b705', '');
+//       _rees46Plugin.initialize('da2668fe479fa866e9d888d976db914a', '');
 
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
+//     } on PlatformException {
+//       platformVersion = 'Failed to get platform version.';
+//     }
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-  }
+//     // If the widget was removed from the tree while the asynchronous platform
+//     // message was in flight, we want to discard the reply rather than calling
+//     // setState to update our non-existent appearance.
+//     if (!mounted) return;
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: InkWell(
-              onTap: () {
-                initPlatformState();
-              },
-              child: const Text('Plugin example app')),
-        ),
-        body: Center(
-          child: InkWell(onTap: () {
-            _rees46Plugin.track("VIEW", '12333');
-            _rees46Plugin.recommend("b92d1c9dcd16ecfb83fb1335bc5e2464", false, "", "");
-          },child: Text('Running on: $_platformVersion\n')),
-        ),
-      ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: InkWell(
+//               onTap: () {
+//                 initPlatformState();
+//               },
+//               child: const Text('Plugin example app')),
+//         ),
+//         body: Center(
+//           child: InkWell(onTap: () {
+//             _rees46Plugin.track("VIEW", '12333');
+//             _rees46Plugin.recommend("b92d1c9dcd16ecfb83fb1335bc5e2464", false, "", "");
+//           },child: Text('Running on: $_platformVersion\n')),
+//         ),
+//       ),
+//     );
+//   }
+// }
--- lib/rees46_controller.dart
+++ lib/rees46_controller.dart
@@ -1,7 +1,7 @@
 import 'rees46_plugin.g.dart';
 
 abstract class IRees46Controller {
-  void initialize(String shopID, String? apiDomain);
+  void initialize(String shopID, String? apiDomain, String? token);
 
   void track(
     String trackEvent,
@@ -35,9 +35,11 @@ class Rees46Controller implements IRees46Controller, Rees46Receiver {
   final Rees46Sender _sink;
 
   @override
-  void initialize(String shopID, String? apiDomain) => _sink.initialize(
+  void initialize(String shopID, String? apiDomain, String? token) =>
+      _sink.initialize(
         shopID,
         apiDomain,
+        token,
       );
 
   @override
